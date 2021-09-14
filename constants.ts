4749755ab00a23b2a5c069d5c66b8d6668a5b1a6
klazbaba <giwaklaz@gmail.com>
2021-09-14 18:43:39 +0100

    no longer tracking constants file

--- constants.ts
+++
@@ -1,6 +0,0 @@
-export const constants = {
-  APP_ID: '53bad13b34284cb4a23b79faa145fc48',
-  TOKEN:
-    '00653bad13b34284cb4a23b79faa145fc48IABtGRB3Ba1yYW5sz2U2EZV6FGsNCjBVWxnkVur4amUKV8651csAAAAAEAD+bihblShCYQEAAQCUKEJh',
-  CHANNEL_NAME: 'Channel Baba',
-};
